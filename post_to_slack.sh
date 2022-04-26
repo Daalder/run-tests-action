053a83f3e827c4293d48c8dc94e3978ef967fcd8
Vincent van Hoven <vincent@pionect.nl>
2022-04-26 14:15:42 +0200

    [MOVE] Slack message to bash script
    Co-Authored-By: Vincent van Hoven <11269255+vincentvanhoven@users.noreply.github.com>

---
+++ post_to_slack.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+SLACK_BASE_URL="https://slack.com/api/chat.postMessage"
+SLACK_BEARER_TOKEN=$1
+SLACK_CHANNEL=$2
+GIT_STATUS=$3
+GIT_BRANCH=$4
+GIT_SHA=$5
+GIT_COMMIT_MESSAGE=$6
+GIT_AUTHOR_FIRSTNAME=$7
+
+message=""
+
+if [ $GIT_STATUS == "failure" ]; then
+  message+=":no_entry: daalder/feeds tests failed";
+else
+  message+=":white_check_mark: daalder/feeds tests succeeded";
+fi
+
+message+=" for \`$GIT_BRANCH\` @ \`\<https://github.com/Daalder/feeds/commit/$GIT_SHA\>\` by @$GIT_AUTHOR_FIRSTNAME."
+message+=" Commit message: \`$GIT_COMMIT_MESSAGE\`"
+
+curl -X POST \
+  -H "Authorization: Bearer $SLACK_BEARER_TOKEN" \
+  "$SLACK_BASE_URL?channel=$SLACK_CHANNEL&link_names=true&pretty=1" \
+  --data-urlencode "$message"
