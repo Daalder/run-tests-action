600bc56f245ce8eb1e1cfeaa114eaeda79ab873d
Vincent van Hoven <vincent@pionect.nl>
2022-04-26 14:55:34 +0200

    [CHANGE] Only send Slack message for successful test run when previous run failed
    Co-Authored-By: Vincent van Hoven <11269255+vincentvanhoven@users.noreply.github.com>

--- post_to_slack.sh
+++ post_to_slack.sh
@@ -1,29 +1,39 @@
 #!/bin/bash
 
+# Define/get all variables
 SLACK_BASE_URL="https://slack.com/api/chat.postMessage"
 SLACK_BEARER_TOKEN=$1
 SLACK_CHANNEL=$2
-GIT_STATUS=$3
-GIT_BRANCH=$4
-GIT_SHA=$5
-GIT_SHORT_SHA=$6
-GIT_COMMIT_MESSAGE=$7
-GIT_AUTHOR_FIRSTNAME=$8
+ACTION_STATUS=$3
+ACTION_PREVIOUS_STATUS=$4
+GIT_BRANCH=$5
+GIT_SHA=$6
+GIT_SHORT_SHA=$7
+GIT_COMMIT_MESSAGE=$8
+GIT_AUTHOR_FIRSTNAME=$9
 
-message=""
-
-if [ "$GIT_STATUS" == "failure" ]; then
-  message+=":no_entry: daalder/feeds tests failed"
-else
-  message+=":white_check_mark: daalder/feeds tests succeeded"
+# If previous and current action succeeded, there is no need for a Slack notification
+if [ "$ACTION_STATUS" == "success" ] && [ "$ACTION_PREVIOUS_STATUS" == "success" ]; then
+  # Exit script without sending a Slack notification
+  exit 0;
 fi
 
-message+=" for \`$GIT_BRANCH\` @ \`<https://github.com/Daalder/feeds/commit/$GIT_SHA|$GIT_SHORT_SHA>\` by @$GIT_AUTHOR_FIRSTNAME."
+# Define message string
+message=""
+
+# If action failed
+if [ "$ACTION_STATUS" == "failure" ]; then
+  message+=":no_entry: daalder/feeds tests failed for"
+else
+  # If action succeeded
+  message+=":white_check_mark: daalder/feeds tests fixed with"
+fi
+
+# Include branch, url to commit, commiter name, commit message
+message+=" \`$GIT_BRANCH\` @ \`<https://github.com/Daalder/feeds/commit/$GIT_SHA|$GIT_SHORT_SHA>\` by @$GIT_AUTHOR_FIRSTNAME."
 message+=" Commit message: \`$GIT_COMMIT_MESSAGE\`"
 
-echo "$message"
-echo ""
-
+# Post formatted message to Slack
 curl -X POST \
   -H "Authorization: Bearer $SLACK_BEARER_TOKEN" \
   "$SLACK_BASE_URL?channel=$SLACK_CHANNEL&link_names=true&pretty=1" \
