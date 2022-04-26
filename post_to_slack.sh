508e6844c63f55ce3da5105690d2b36a1426220e
Vincent van Hoven <vincent@pionect.nl>
2022-04-26 14:43:29 +0200

    [FIX] Post to slack script
    Co-Authored-By: Vincent van Hoven <11269255+vincentvanhoven@users.noreply.github.com>

--- post_to_slack.sh
+++ post_to_slack.sh
@@ -6,21 +6,25 @@ SLACK_CHANNEL=$2
 GIT_STATUS=$3
 GIT_BRANCH=$4
 GIT_SHA=$5
-GIT_COMMIT_MESSAGE=$6
-GIT_AUTHOR_FIRSTNAME=$7
+GIT_SHORT_SHA=$6
+GIT_COMMIT_MESSAGE=$7
+GIT_AUTHOR_FIRSTNAME=$8
 
 message=""
 
-if [ $GIT_STATUS == "failure" ]; then
-  message+=":no_entry: daalder/feeds tests failed";
+if [ "$GIT_STATUS" == "failure" ]; then
+  message+=":no_entry: daalder/feeds tests failed"
 else
-  message+=":white_check_mark: daalder/feeds tests succeeded";
+  message+=":white_check_mark: daalder/feeds tests succeeded"
 fi
 
-message+=" for \`$GIT_BRANCH\` @ \`\<https://github.com/Daalder/feeds/commit/$GIT_SHA\>\` by @$GIT_AUTHOR_FIRSTNAME."
+message+=" for \`$GIT_BRANCH\` @ \`<https://github.com/Daalder/feeds/commit/$GIT_SHA|$GIT_SHORT_SHA>\` by @$GIT_AUTHOR_FIRSTNAME."
 message+=" Commit message: \`$GIT_COMMIT_MESSAGE\`"
+
+echo "$message"
+echo ""
 
 curl -X POST \
   -H "Authorization: Bearer $SLACK_BEARER_TOKEN" \
   "$SLACK_BASE_URL?channel=$SLACK_CHANNEL&link_names=true&pretty=1" \
-  --data-urlencode "$message"
+  --data-urlencode "text=$message"
